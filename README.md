d275cb46a04812303f30028c5b73f67ff58775f7
Miguel Ruiz <mruiz@agetic.gob.bo>
2020-06-21 14:23:28 -0400

    Lint and update doc

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Codeforces input and output contest scrapper
-## Get all input and output test cases of codeforces contest
+## Get all input and output test cases of codeforces contest in files .txt
 
 ### Requirements
 - Python 3
@@ -23,8 +23,8 @@ $ python3.8 cf_scrapper.py
 Enter contest id: 
 ```
 
-and enter contest id
+Enter contest id
 
 *** Ej. 1370 is contest id in https://codeforces.com/contest/1370/problems URL
 
-The program do scrapes in page and get all TC. Optionally copy your template to each directory of problems 
+Ready to solve contest
--- cf_scrapper.py
+++ cf_scrapper.py
@@ -1,95 +1,93 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import requests
 import urllib.request
 import os
 from bs4 import BeautifulSoup
 from shutil import copyfile
 
-# to config 
-configPath = "/home/user/" # Directory for save TC contest
-pathToTemplate = None # Optional - path to template file Example: /home/io.cpp
+# to config
 
-if pathToTemplate != None and type(pathToTemplate) is not str:
-	print('WARNING, pathToTemplate config must be a string')
+configPath = '/home/user/'  # Directory for save TC contest
+pathToTemplate = None  # Optional - path to template file Example: /home/io.cpp
+
+if pathToTemplate is not None and type(pathToTemplate) is not str:
+    print('WARNING, pathToTemplate config must be a string')
 elif type(pathToTemplate) is str:
-	path_ = pathToTemplate.split('/')
-	fileName = path_[len(path_) - 1]
-	extension = fileName.split('.')[1]
+    path_ = pathToTemplate.split('/')
+    fileName = path_[len(path_) - 1]
+    extension = fileName.split('.')[1]
 
-id_contest = input("Enter contest id: ")
+id_contest = input('Enter contest id: ')
 pathContestSolutions = configPath + id_contest
 
 
 def createFileInDirectory(dir_, content_):
-	file_ = open(dir_, 'w')
-	file_.writelines(content_)
-	file_.close()
+    file_ = open(dir_, 'w')
+    file_.writelines(content_)
+    file_.close()
+
 
 def deleteInitialAndEndCRLF(string_):
-	l = 0
-	r = len(string_)
-	if len(string_) > 2:
-		if (ord (string_[0]) == 13 or ord (string_[0]) == 10):
-			l+=1
-		if (ord (string_[r- 1] ) == 13 or ord (string_[r -1]) == 10):
-			r-=1
-	return string_[l:r]
+    l = 0
+    r = len(string_)
+    if len(string_) > 2:
+        if ord(string_[0]) == 13 or ord(string_[0]) == 10:
+            l += 1
+        if ord(string_[r - 1]) == 13 or ord(string_[r - 1]) == 10:
+            r -= 1
+    return string_[l:r]
+
+
 try:
-	url = 'https://codeforces.com/contest/'+ str(id_contest)+ '/problems'
-	print('REQUEST TO : ', url)
-	response = requests.get(url)
-	""" For test 
-	outF = open("codeforces.txt", "w")
-	outF.writelines(response.text)
-	outF.close()
-	"""
-	if not os.path.exists(pathContestSolutions):
-		os.mkdir(pathContestSolutions)
-	else:
-		print('WARNING :  Already exist contest path directory')
-	htmlToScrap = BeautifulSoup(response.content, 'html.parser')
-	divproblems = htmlToScrap.find_all('div', class_='problemindexholder')
-	problemsLevel = []
-	existDirec = False
-	for i in divproblems:
-		problemsLevel.append(i['problemindex'])
-		pathProblem = pathContestSolutions + '/' + i['problemindex'] + '/'
-		if not os.path.exists(pathProblem):
-			os.mkdir(pathProblem)
-		else:
-			existDirec = True
-		if type(pathToTemplate) is str:
-			templateProblem = pathProblem + id_contest + i['problemindex'] + '.' + extension
-			copyfile(pathToTemplate, templateProblem )
-			print('Template copied:', pathProblem + id_contest + i['problemindex'] + '.' + extension) 
-		inputTC = i.find_all('div', class_ ='input')
-		problemIndex = 0
-		for ii in inputTC:
-			problemIndex +=1
-			itemInput = ii.find('pre')
-			problemInputRaw = itemInput.text
-			lines = problemInputRaw.split("\n")
-			problemTCFIleName = 'in' + str(problemIndex) + '.txt'
-			filepath = os.path.join(pathProblem, problemTCFIleName)
-			toWrite = deleteInitialAndEndCRLF(problemInputRaw)
-			createFileInDirectory(filepath, toWrite)
-		outputTC = i.find_all('div', class_ ='output')
-		problemIndex = 0
-		for ind, e in enumerate(outputTC):
-			itemOutput = e.find('pre')
-			problemOutputRaw = itemOutput.text
-			lines = problemOutputRaw.split("\n")
-			problemFileTCFileName = 'out' + str(ind + 1) + '.txt'
-			filepath = os.path.join(pathProblem, problemFileTCFileName)
-			toWrite = deleteInitialAndEndCRLF(problemOutputRaw)
-			createFileInDirectory(filepath, toWrite)
+    url = 'https://codeforces.com/contest/' + str(id_contest) + '/problems'
+    print('REQUEST TO : ', url)
+    response = requests.get(url)
+    if not os.path.exists(pathContestSolutions):
+        os.mkdir(pathContestSolutions)
+    else:
+        print('WARNING :  Already exist contest path directory')
+    htmlToScrap = BeautifulSoup(response.content, 'html.parser')
+    divproblems = htmlToScrap.find_all('div', class_='problemindexholder')
+    problemsLevel = []
+    existDirec = False
+    for i in divproblems:
+        problemsLevel.append(i['problemindex'])
+        pathProblem = pathContestSolutions + '/' + i['problemindex'] + '/'
+        if not os.path.exists(pathProblem):
+            os.mkdir(pathProblem)
+        else:
+            existDirec = True
+        if type(pathToTemplate) is str:
+            templateProblem = pathProblem + id_contest + i['problemindex'] + '.' + extension
+            copyfile(pathToTemplate, templateProblem)
+            print(
+                'Template copied:',
+                pathProblem + id_contest + i['problemindex'] + '.' + extension)
+        inputTC = i.find_all('div', class_='input')
+        problemIndex = 0
+        for ii in inputTC:
+            problemIndex += 1
+            itemInput = ii.find('pre')
+            problemInputRaw = itemInput.text
+            lines = problemInputRaw.split('\n')
+            problemTCFIleName = 'in' + str(problemIndex) + '.txt'
+            filepath = os.path.join(pathProblem, problemTCFIleName)
+            toWrite = deleteInitialAndEndCRLF(problemInputRaw)
+            createFileInDirectory(filepath, toWrite)
+        outputTC = i.find_all('div', class_='output')
+        problemIndex = 0
+        for (ind, e) in enumerate(outputTC):
+            itemOutput = e.find('pre')
+            problemOutputRaw = itemOutput.text
+            lines = problemOutputRaw.split('\n')
+            problemFileTCFileName = 'out' + str(ind + 1) + '.txt'
+            filepath = os.path.join(pathProblem, problemFileTCFileName)
+            toWrite = deleteInitialAndEndCRLF(problemOutputRaw)
+            createFileInDirectory(filepath, toWrite)
 
-	print('The problems test cases created in: ' , pathContestSolutions)
-	print('id contest : ', id_contest)
-	print('PROBLEM LEVELS : ', problemsLevel)
-
+    print('The problems test cases created in: ', pathContestSolutions)
+    print('id contest : ', id_contest)
+    print('PROBLEM LEVELS : ', problemsLevel)
 except Exception as e:
-	raise e
-
-
-
-
+    print(e)
